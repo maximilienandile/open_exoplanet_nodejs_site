ef6c2bd7c7b9acf7573464d70d2c641263f6cb71
Maximilien Andile <hello@maximilien-andile.com>
2016-10-27 10:34:58 +0200

    wip on layout

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ router.get('/', function (req, res) {
   var db = req.app.locals.db
   get_data_from_db.get_last_updated_and_discovered_planets(db).then(function (docs) {
     res.render('index', {
-      data: docs
+      data: docs, title : "A list of all exoplanets"
     });
 
   }, function (err) {
@@ -23,7 +23,7 @@ router.get('/random', function (req, res) {
   var db = req.app.locals.db
   get_data_from_db.get_a_random_planet(db).then(function(planet){
     res.render('planet', {
-      data: planet
+      data: planet, title : "Random Exoplanet"
     });
   },function(err){
     res.render('app_error', {
@@ -39,8 +39,7 @@ router.get('/planet/:planet_name', function (req, res) {
   var db = req.app.locals.db
   get_data_from_db.get_a_planet_by_name_slug(db,req.params.planet_name).then(function(planet){
     res.render('planet', {
-      data: planet
-    });
+      data: planet, title : ""+planet.display_name+" - Informations about the exoplanet"})
   }, function(err){
     res.render('app_error', {
       message: err
